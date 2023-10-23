324ed0bc1be0ae47906d7b53f2f0ad3a8d5c6215
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-10-22 21:37:15 -0400

    Initial project commit with some code

---
+++ src/main/java/org/example/Utils.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Utils {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static char getCharInput() {
+        String userInput = scanner.next();
+        scanner.nextLine();
+        return userInput.toUpperCase().charAt(0);
+    }
+
+    public static String getStringPrompt(Scanner scanner, String prompt){
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+}
