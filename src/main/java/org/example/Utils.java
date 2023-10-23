def3f80c8a8a2a40e6f9b07550b18bff5c26eda1
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-10-23 14:56:32 -0400

    Added getDate and getTime for Main.java

--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Utils {
@@ -10,8 +13,50 @@ public class Utils {
         return userInput.toUpperCase().charAt(0);
     }
 
-    public static String getStringPrompt(Scanner scanner, String prompt){
+    public static String getStringPrompt(String prompt){
         System.out.println(prompt);
         return scanner.nextLine();
+    }
+
+    public static LocalDate getDate(String prompt){
+        while (true){
+            System.out.println(prompt);
+            String userInput = scanner.nextLine();
+
+            if (userInput.isEmpty()) {
+                return LocalDate.now();
+            }
+
+            if(!userInput.contains("-")){
+                System.out.println("Please try again. It's incorrect");
+            } else {
+                try {
+                    return LocalDate.parse(userInput);
+                } catch (DateTimeException e){
+                    System.out.println("Invalid date format. Please try again");
+                }
+            }
+        }
+    }
+
+    public static LocalTime getTime(String prompt){
+        while (true){
+            System.out.println(prompt);
+            String userInput = scanner.nextLine();
+
+            if (userInput.isEmpty()) {
+                return LocalTime.now();
+            }
+
+            if(!userInput.contains(":")){
+                System.out.println("Please try again. It's incorrect");
+            } else {
+                try {
+                    return LocalTime.parse(userInput);
+                } catch (Exception e ){
+                    System.out.println("Invalid date format. Please try again");
+                }
+            }
+        }
     }
 }
