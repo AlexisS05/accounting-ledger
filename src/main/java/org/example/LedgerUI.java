fbf67ca18339ee29bda9e4e21459e74932fb6258
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-10-26 18:12:25 -0400

    Fixed search by vendor

--- src/main/java/org/example/LedgerUI.java
+++ src/main/java/org/example/LedgerUI.java
@@ -69,18 +69,18 @@ public class LedgerUI {
         boolean data; // Always true to show data
         for (Transaction transaction : transactions) {
             if (filter.equals("Entries")) {
-                System.out.println(transaction);
-            } else if (filter.equals("Deposits")){
-                data = transaction.getInputAmount() > 0;
+                printTransaction(transaction);
+            } else if (filter.equals("Deposits")) {
+                data = transaction.getAmount() > 0;
 
                 if (data) {
-                    System.out.println(transaction);
+                    printTransaction(transaction);
                 }
             } else if (filter.equals("Payments")) {
-                data = transaction.getInputAmount() < 0;
+                data = transaction.getAmount() < 0;
 
                 if (data) {
-                    System.out.println(transaction);
+                    printTransaction(transaction);
                 }
             }
         }
@@ -105,7 +105,7 @@ public class LedgerUI {
                         year2 = now.getYear();
                         // Compare it? date == date2
                         if (date == date2 && year == year2) {
-                            System.out.println(transaction);
+                            printTransaction(transaction);
                         }
                     }
                     break;
@@ -117,7 +117,7 @@ public class LedgerUI {
                         year2 = now.getYear();
                         // Compare it? date == date2
                         if (date == date2 && year == year2) {
-                            System.out.println(transaction);
+                            printTransaction(transaction);
                         }
                     }
                     break;
@@ -126,7 +126,7 @@ public class LedgerUI {
                         date = transaction.getDateInput().getYear();
                         date2 = now.getYear();
                         if (date == date2) {
-                            System.out.println(transaction);
+                            printTransaction(transaction);
                         }
                     }
                     break;
@@ -136,45 +136,70 @@ public class LedgerUI {
                         date2 = now.minusYears(1).getYear();
                         // Compare it? date == date2
                         if (date == date2) {
-                            System.out.println(transaction);
+                            printTransaction(transaction);
                         }
                     }
                     break;
                 case '5':
-                    String inputVendor = Utils.getStringInput("Enter Vendor Name: ");
+                    String inputVendor = Utils.getStringInput("Enter Vendor Name: ").toLowerCase();
                     for (Transaction transaction : transactions
                     ) {
-                        if (transaction.inputVendor().contains(inputVendor)) {
-                            System.out.println(transaction);
+                        if (transaction.inputVendor().toLowerCase().contains(inputVendor)) {
+                            printTransaction(transaction);
                         }
                     }
                     break;
                 case '6':
-                    // Custom Search
-                    System.out.println("Custom Search: ");
-                    LocalDate startDate = Utils.getDateCustom("Enter a start date in (YYYY-MM-DD) or leave blank");
-                    LocalDate endDate = Utils.getDateCustom("Enter a end date in (YYYY-MM-DD) or leave blank");
-                    String description = Utils.getStringInput("Enter a description: ").toLowerCase();
-                    String vendor = Utils.getStringInput("Enter a vendor: ").toLowerCase();
-                    String amountString = Utils.getStringInput("Enter an amount: "); // Evaluate input to String for isEmpty
-                    double amount = amountString.isEmpty() ? 0 : Double.parseDouble(amountString); // Evaluate if amount is Empty.
-
-                    System.out.println("Here is your custom search: ");
-                    for (Transaction transaction: transactions
-                         ) {
-                        boolean isStartDate = startDate == null || !transaction.getDateInput().isBefore(startDate); // Evaluates transaction date is not before the startDate or if date is null
-                        boolean isEndDate = endDate == null || !transaction.getDateInput().isAfter(endDate); // Evaluates transactions date is not after the endDate or if date is null
-                        boolean isDescription = transaction.getInputDescription().contains(description);
-                        boolean isVendor = transaction.getInputVendor().contains(vendor);
-                        boolean isAmount = transaction.getInputAmount() == amount || amount == 0;
-                        if(isStartDate && isEndDate && isDescription && isVendor && isAmount){
-                            System.out.println(transaction);
-                        }
-                    }
+                    customSearch();
                     break;
                 case '7':
                     return;
             }
         }
+    }
+
+    public static void displayReports(String filter) {
+        int date;
+        int date2;
+        int year;
+        int year2;
+        LocalDate now = LocalDate.now();
+
+        for (Transaction transaction : transactions
+        ) {
+        }
+    }
+
+    public static void customSearch() {
+        // Custom Search
+        System.out.println("Custom Search: ");
+        LocalDate startDate = Utils.getDateCustom("Enter a start date in (YYYY-MM-DD) or leave blank");
+        LocalDate endDate = Utils.getDateCustom("Enter a end date in (YYYY-MM-DD) or leave blank");
+        String description = Utils.getStringInput("Enter a description: ").toLowerCase();
+        String vendor = Utils.getStringInput("Enter a vendor: ").toLowerCase();
+        String amountString = Utils.getStringInput("Enter an amount: "); // Evaluate input to String for isEmpty
+        double amount = amountString.isEmpty() ? 0 : Double.parseDouble(amountString); // Evaluate if amount is Empty.
+
+        System.out.println("Here is your custom search: ");
+        for (Transaction transaction : transactions
+        ) {
+            boolean isStartDate = startDate == null || !transaction.getDateInput().isBefore(startDate); // Evaluates transaction date is not before the startDate or if date is null
+            boolean isEndDate = endDate == null || !transaction.getDateInput().isAfter(endDate); // Evaluates transactions date is not after the endDate or if date is null
+            boolean isDescription = transaction.getDescription().contains(description);
+            boolean isVendor = transaction.getVendor().contains(vendor);
+            boolean isAmount = transaction.getAmount() == amount || amount == 0;
+            if (isStartDate && isEndDate && isDescription && isVendor && isAmount) {
+                printTransaction(transaction);
+            }
+        }
+    }
+    private static void printTransaction(Transaction item) {
+        System.out.printf("%-15s %-15s %-30s %-25s %15.2f\n",
+                item.getDateInput(),
+                item.getTimeInput(),
+                item.getDescription(),
+                item.getVendor(),
+                item.getAmount()
+        );
     }
 }
