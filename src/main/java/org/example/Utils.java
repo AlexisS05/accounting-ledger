11e213a9df039ea8ece137638da3c1e887de07f9
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-10-27 00:19:59 -0400

    Added defaultCSV method to create file, refactored other methods for errors

--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.*;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -8,7 +9,6 @@ import java.util.Scanner;
 
 public class Utils {
     static Scanner scanner = new Scanner(System.in);
-
     public static char getCharInput() {
         String userInput = scanner.nextLine().toUpperCase().trim();
         if (!userInput.isEmpty()) {
@@ -24,8 +24,17 @@ public class Utils {
     }
 
     public static double getDoubleInput(String prompt) {
-        System.out.println(prompt);
-        return scanner.nextDouble();
+        while (true) {
+            System.out.println(prompt);
+            if (scanner.hasNextDouble()) {
+                double input = scanner.nextDouble();
+                scanner.nextLine();
+                return input;
+            } else {
+                System.out.println("Invalid input. Please enter a valid number.");
+                scanner.nextLine();
+            }
+        }
     }
 
     public static LocalDate getDate(String prompt) {
@@ -35,6 +44,30 @@ public class Utils {
 
             if (userInput.isEmpty()) {
                 return LocalDate.now();
+            }
+
+            try {
+                LocalDate parsedDate = LocalDate.parse(userInput);
+                LocalDate currentDate = LocalDate.now();
+
+                if (parsedDate.isAfter(currentDate)) {
+                    System.out.println("You entered a date in the future. Please enter a valid date.");
+                } else {
+                    return parsedDate;
+                }
+            } catch (DateTimeException e) {
+                System.out.println("Invalid date format. Please try again");
+            }
+        }
+    }
+
+    public static LocalDate getDateCustom(String prompt) {
+        while (true) {
+            System.out.println(prompt);
+            String userInput = scanner.nextLine().trim();
+
+            if (userInput.isEmpty()) {
+                return null;
             }
 
             try {
@@ -64,5 +97,22 @@ public class Utils {
                 System.out.println("Invalid date format. Please try again");
             }
         }
+    }
+
+    public static BufferedWriter createDefaultCSV(String fileName) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            File file = new File(fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+                System.out.println("CSV file created!");
+            }
+            // Append data to the existing file or create a new one if it doesn't exist
+            FileWriter fileWriter = new FileWriter(file, true);
+            bufferedWriter = new BufferedWriter(fileWriter);
+        } catch (IOException e) {
+            System.out.println("Error creating or opening the CSV file!");
+        }
+        return bufferedWriter;
     }
 }
