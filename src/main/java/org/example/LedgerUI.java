962255fa39a70b1d212a3dbafcfde14c9cb56dd4
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-10-27 00:52:21 -0400

    Added method to print titles. Added color for title, Refactored some code

--- src/main/java/org/example/LedgerUI.java
+++ src/main/java/org/example/LedgerUI.java
@@ -25,13 +25,15 @@ public class LedgerUI {
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
-            Utils.createDefaultCSV();
+            System.exit(0);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         // Sort our transactions ArrayList in ascending order before returning it
-        Comparator<Transaction> compareByDate = Comparator.comparing(Transaction::getDateInput).reversed();
-        Comparator<Transaction> compareByTime = Comparator.comparing(Transaction::getTimeInput).reversed();
+        Comparator<Transaction> compareByDate = Comparator.comparing(Transaction::getDate).reversed();
+        Comparator<Transaction> compareByTime = Comparator.comparing(Transaction::getTime).reversed();
+        Comparator<Transaction> compareByDescription = Comparator.comparing(Transaction::getDescription).reversed();
+        transactions.sort(compareByDescription);
         transactions.sort(compareByDate.thenComparing(compareByTime));
 
         return transactions;
@@ -41,7 +43,9 @@ public class LedgerUI {
 
     public static void ledgerMenu() {
         while (true) {
-            System.out.println("LEDGER MENU");
+            System.out.print("\u001B[31m");
+            System.out.printf("%20s\n", "LEDGER MENU");
+            System.out.print("\u001B[0m");
             System.out.println("A) Display all entries \nD) Display deposits \nP) Display payments \nR) Go to Reports \nH) Return Home");
             char input = Utils.getCharInput();
             switch (input) {
@@ -55,7 +59,7 @@ public class LedgerUI {
                     displayEntries("Payments");
                     break;
                 case 'R':
-                    reportsMenu();
+                    Reports.reportsMenu();
                     break;
                 case 'H':
                     return;
@@ -67,18 +71,17 @@ public class LedgerUI {
 
     public static void displayEntries(String filter) {
         boolean data; // Always true to show data
-        for (Transaction transaction : transactions) {
+        printTransactionTitle();
+        for (Transaction transaction : transactions) { // Handles if true data will show
             if (filter.equals("Entries")) {
                 printTransaction(transaction);
             } else if (filter.equals("Deposits")) {
                 data = transaction.getAmount() > 0;
-
                 if (data) {
                     printTransaction(transaction);
                 }
             } else if (filter.equals("Payments")) {
                 data = transaction.getAmount() < 0;
-
                 if (data) {
                     printTransaction(transaction);
                 }
@@ -86,125 +89,23 @@ public class LedgerUI {
         }
     }
 
-    public static void reportsMenu() {
-        while (true) {
-            System.out.println("REPORTS MENU");
-            System.out.println("1) Month To Date  \n2) Previous Month \n3) Year To Date \n4) Previous Year \n5) Search By Vendor \n6) Custom Search \n7) Return to Ledger");
-            String vendor = "";
-            char input = Utils.getCharInput();
-            switch (input) {
-                case '1':
-                    displayReports("Month To Date", vendor);
-                    break;
-                case '2':
-                    displayReports("Previous Month", vendor);
-                    break;
-                case '3':
-                    displayReports("Year To Date", vendor);
-                    break;
-                case '4':
-                    displayReports("Previous Year", vendor);
-                    break;
-                case '5':
-                    String inputVendor = Utils.getStringInput("Enter Vendor Name: ").toLowerCase();
-                    displayReports("", inputVendor);
-                    break;
-                case '6':
-                    customSearch();
-                    break;
-                case '7':
-                    return;
-            }
-        }
-    }
-
-    public static void displayReports(String filter, String vendor) {
-        int date;
-        int date2;
-        int year;
-        int year2;
-        if (!vendor.isEmpty()) {
-            filter = vendor;
-        }
-        LocalDate now = LocalDate.now();
-
-        for (Transaction transaction : transactions) {
-            if (filter.equals("Month To Date")) {
-                date = transaction.getDateInput().getMonthValue();
-                date2 = now.getMonthValue();
-                year = transaction.getDateInput().getYear();
-                year2 = now.getYear();
-                // Compare it? date == date2
-                if (date == date2 && year == year2) {
-                    printTransaction(transaction);
-                }
-            } else if (filter.equals("Previous Month")) {
-                date = transaction.getDateInput().getMonthValue();
-                date2 = now.minusMonths(1).getMonthValue();
-                year = transaction.getDateInput().getYear();
-                year2 = now.getYear();
-                // Compare it? date == date2
-                if (date == date2 && year == year2) {
-                    printTransaction(transaction);
-                }
-            } else if (filter.equals("Year To Date")) {
-                date = transaction.getDateInput().getYear();
-                date2 = now.getYear();
-                if (date == date2) {
-                    printTransaction(transaction);
-                }
-            } else if (filter.equals("Previous Year")) {
-                date = transaction.getDateInput().getYear();
-                date2 = now.minusYears(1).getYear();
-                // Compare it? date == date2
-                if (date == date2) {
-                    printTransaction(transaction);
-                }
-            } else {
-                if (transaction.inputVendor().toLowerCase().contains(vendor)) {
-                    printTransaction(transaction);
-                }
-            }
-        }
-    }
-
-    public static void customSearch() {
-        // Custom Search
-        System.out.println("Custom Search: ");
-        LocalDate startDate = Utils.getDateCustom("Enter a start date in (YYYY-MM-DD) or leave blank");
-        LocalDate endDate = Utils.getDateCustom("Enter a end date in (YYYY-MM-DD) or leave blank");
-        String description = Utils.getStringInput("Enter a description: ").toLowerCase();
-        String vendor = Utils.getStringInput("Enter a vendor: ").toLowerCase();
-        String amountString = Utils.getStringInput("Enter an amount: "); // Evaluate input to String for isEmpty
-        double amount = amountString.isEmpty() ? 0 : Double.parseDouble(amountString); // Evaluate if amount is Empty.
-
-        boolean ifFound = false; // Evaluate if there will be a result
-
-        System.out.println("Here is your custom search: ");
-        for (Transaction transaction : transactions
-        ) {
-            boolean isStartDate = startDate == null || !transaction.getDateInput().isBefore(startDate); // Evaluates transaction date is not before the startDate or if date is null
-            boolean isEndDate = endDate == null || !transaction.getDateInput().isAfter(endDate); // Evaluates transactions date is not after the endDate or if date is null
-            boolean isDescription = transaction.getDescription().toLowerCase().contains(description);
-            boolean isVendor = transaction.getVendor().toLowerCase().contains(vendor);
-            boolean isAmount = transaction.getAmount() == amount || amount == 0;
-            if (isStartDate && isEndDate && isDescription && isVendor && isAmount) {
-                printTransaction(transaction);
-                ifFound = true;
-            }
-        }
-        if(!ifFound){
-            System.out.println("We couldn't find that. Sorry.");
-        }
-    }
-
-    private static void printTransaction(Transaction item) {
+    public static void printTransaction(Transaction transaction) { // To print data
         System.out.printf("%-15s %-15s %-30s %-25s %15.2f\n",
-                item.getDateInput(),
-                item.getTimeInput(),
-                item.getDescription(),
-                item.getVendor(),
-                item.getAmount()
+                transaction.getDate(),
+                transaction.getTime(),
+                transaction.getDescription(),
+                transaction.getVendor(),
+                transaction.getAmount()
+        );
+    }
+
+    public static void printTransactionTitle() { // To print titles
+        System.out.printf("%-15s %-15s %-30s %-25s %15s\n",
+                "Date",
+                "Time",
+                "Description",
+                "Vendor",
+                "Amount"
         );
     }
 }
